8c24015e516ecbef9c32d329e9133905324ba16a
JohGuentner <3506359+johguentner@users.noreply.github.com>
2023-11-20 11:37:36 +0100

    Create IpWhitelistingMiddleware.php

---
+++ app/Http/Middleware/IpWhitelistingMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IpWhitelistingMiddleware
+{
+
+    public function handle($request, Closure $next)
+    {
+        $whitelist = env('ACCESS_WHITELIST');
+
+       $ipAddresses = explode(';', $whitelist);
+
+      if (! in_array($request->ip(), $ipAddresses)) {
+        // here instead of checking a single ip address we can do collection of ips
+        //address in constant file and check with in_array function
+            return abort(403);
+        }
+
+        return $next($request);
+    }
+
+}
